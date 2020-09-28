0d3689074d87dceb43f354ca2517afa34495b71c
1311535276@qq.com <1311535276@qq.com>
2020-09-28 17:28:23 +0800

    1.增加分页组件pagination.vue 2.大章隔离页面使用分页组件,可自定义初始每页五条,最多显示八个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,117 +1,135 @@
 <template>
     <div>
         <p>
-    <button class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-times red2"></i>
-        刷新
-    </button>
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+                <i class="ace-icon fa fa-refresh"></i>
+                刷新
+            </button>
         </p>
-    <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
 
-            <th>ID</th>
-            <th>名称</th>
-            <th>课程ID</th>
-            <th>操作</th>
+        <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+        <table id="simple-table" class="table  table-bordered table-hover">
+            <thead>
+            <tr>
 
-        </tr>
-        </thead>
+                <th>ID</th>
+                <th>名称</th>
+                <th>课程ID</th>
+                <th>操作</th>
 
-        <tbody>
-        <tr v-for="chapter in chapters">
-            <td>{{chapter.id}}</td>
-            <td>{{chapter.name}}</td>
-            <td>{{chapter.courseId}}</td>
+            </tr>
+            </thead>
 
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                    <button class="btn btn-xs btn-success">
-                        <i class="ace-icon fa fa-check bigger-120"></i>
-                    </button>
+            <tbody>
+            <tr v-for="chapter in chapters">
+                <td>{{chapter.id}}</td>
+                <td>{{chapter.name}}</td>
+                <td>{{chapter.courseId}}</td>
 
-                    <button class="btn btn-xs btn-info">
-                        <i class="ace-icon fa fa-pencil bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-danger">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-warning">
-                        <i class="ace-icon fa fa-flag bigger-120"></i>
-                    </button>
-                </div>
-
-                <div class="hidden-md hidden-lg">
-                    <div class="inline pos-rel">
-                        <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown"
-                                data-position="auto">
-                            <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                <td>
+                    <div class="hidden-sm hidden-xs btn-group">
+                        <button class="btn btn-xs btn-success">
+                            <i class="ace-icon fa fa-check bigger-120"></i>
                         </button>
 
-                        <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                            <li>
-                                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+                        <button class="btn btn-xs btn-info">
+                            <i class="ace-icon fa fa-pencil bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-danger">
+                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-warning">
+                            <i class="ace-icon fa fa-flag bigger-120"></i>
+                        </button>
+                    </div>
+
+                    <div class="hidden-md hidden-lg">
+                        <div class="inline pos-rel">
+                            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown"
+                                    data-position="auto">
+                                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                                <li>
+                                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
                                                                     <span class="blue">
                                                                         <i class="ace-icon fa fa-search-plus bigger-120"></i>
                                                                     </span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                                <li>
+                                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
                                                                     <span class="green">
                                                                         <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
                                                                     </span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                                <li>
+                                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
                                                                     <span class="red">
                                                                         <i class="ace-icon fa fa-trash-o bigger-120"></i>
                                                                     </span>
-                                </a>
-                            </li>
-                        </ul>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
+    //引入外部文件一
+    import Pagination from "../../components/pagination"
+
     export default {
-        name: 'chapter',
+        //引入外部文件二
+        components: {Pagination},
+        name: "chapter",
         data:function(){
             return {
                 chapters:[]
             }
         },
         mounted: function () {
+
             let _this=this;
+            _this.$refs.pagination.size=5;
             // 调用 list()方法
-            _this.list();
+            //初始化第一页
+            _this.list(1);
             //sidebar激活样式方法一
             // this.$parent.activeSidebar("business-sidebar");
         },
         methods: {
-            list(){
+            list(page){
                 let _this=this;
                 // 获取list 从后台获取sql数据
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',
                     {
-                        page: 1,
-                        size: 1
+                        // 设置页数 (前端传进来的参数)
+                        page: page,
+                        // $refs.参数 意思是获取子组件 就是后面的参数
+                        //pagination是外部文件 在上面有引入
+                        size: _this.$refs.pagination.size,
                     }).then((response)=>{
                     // 数据存储在response
                     console.log("查询大章列表结果:",response);
-                    // 就数据放进前端data(双面数据)
+                    // 就数据放进前端data(双面数据) 渲染数据
+                    //获取到数据 存进data ,data点出list(记录)  渲染数据
                     _this.chapters =response.data.list;
+                    //获取到数据 存进data ,data点出total(总页数) 渲染数据
+
+                    _this.$refs.pagination.render(page,response.data.total);
+
                 })
             }
         }
