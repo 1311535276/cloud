f9104c2624e3e6c8ddd0de7dcc2754f1cba4e872
hShen <1311535276@qq.com>
2021-04-22 20:04:37 +0800

    1·利用setInterval完成自动保存功能 2·增加日期格式化方法 3·7-5结束了

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -173,7 +173,12 @@
             <div class="modal-body">
               <form class="form-horizontal">
                 <div class="form-group">
-                  <div class="col-1g-12">
+                <div class="col-lg-12">
+                  {{saveContentLabel}}
+                </div>
+            </div>
+                <div class="form-group">
+                  <div class="col-lg-12">
                     <div id="content"></div>
                   </div>
                 </div>
@@ -207,6 +212,7 @@
                 COURSE_STATUS: COURSE_STATUS,
                 categorys: [],
                 tree: {},
+              saveContentLabel:"",
             }
         },
         mounted: function () {
@@ -345,7 +351,7 @@
                 SessionStorage.set(SESSION_KEY_COURSE, course);
                 _this.$router.push("/business/content");
             },
-          // 这里
+          // 这里 打开内容编辑
             editContent(course) {
               let _this = this;
               let id = course.id;
@@ -356,6 +362,7 @@
               });
               //先清空历史文本
               $("#content").summernote('code', '');
+            _this.saveContentLabel="",
               Loading.show();
               _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response) => {
                 Loading.hide();
@@ -366,6 +373,14 @@
                     $("#course-content-modal").modal({backdrop: ' static', keyboard: false});
                     $("#content").summernote('code', resp.content.content);
                   }
+                  // 定时自动保存
+                  let saveContentInterval = setInterval(function() {
+                    _this.saveContent();
+                  }, 5000);
+                  //关闭内容框时,清空自动保存任务
+                  $('#course-content-modal').on('hidden.bs.modal', function (e) {
+                    clearInterval(saveContentInterval);
+                  })
                 } else {
                   Toast.warning(resp.message);
                 }
@@ -384,10 +399,10 @@
               Loading.hide();
               let resp = response.data;
               if (resp.success) {
-                Toast.success("内容保存成功");
-                // let now = Tool.dateFormat("yyyy-MM-dd hh:mm:ss");
+                // Toast.success("内容保存成功");
+                let now = Tool.dateFormat("yyyy-MM-dd hh:mm:ss");
                 // let now = Tool.dateFormat("mm:ss");
-                // _this.saveContentLabel = "最后保存时间：" + now;
+                _this.saveContentLabel = "最后保存时间：" + now;
               } else {
                 Toast.warning(resp.message);
               }
