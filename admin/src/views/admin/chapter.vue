e5bc35c3dbff667798cfdc03c83990eef5676cf9
hShen <1311535276@qq.com>
2021-04-23 01:29:41 +0800

    1,home隐藏大章和小节 2,7-6

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -118,7 +118,7 @@
             //初始化第一页
             _this.list(1);
             //sidebar激活样式方法一
-            // this.$parent.activeSidebar("business-sidebar");
+            this.$parent.activeSidebar("business-course-sidebar");
 
         },
         methods: {
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -188,7 +188,7 @@
             //初始化第一页
             _this.list(1);
             //sidebar激活样式方法一
-            // this.$parent.activeSidebar("business-sidebar");
+            this.$parent.activeSidebar("business-course-sidebar");
         },
         methods: {
             add() {
