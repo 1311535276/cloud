36f2992ace77fdba8832082526a8ee666e33caf4
hShen <1311535276@qq.com>
2021-04-28 10:53:21 +0800

    1.文件上传功能开发：增加上传文件类型的判断 2.8-3

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -97,7 +97,7 @@
                     <i class="ace-icon fa fa-upload"></i>
                     上传头像
                   </button>
-                  <input type="file" v-on:change="uploadImage()" id="file-upload-input" class="hidden"/>
+                  <input type="file" v-on:change="uploadImage()" id="file-upload-input" class="hidden" ref="file"/>
                   <!--  图片显示-->
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
@@ -254,6 +254,31 @@ export default {
     uploadImage() {
       let _this = this;
       let formData = new window.FormData();
+      let file = _this.$refs.file.files[0];
+      //判断文件格式
+      //定义我需要的文件后缀
+      let suffixs = ["jpg", "jpeg", "png"];
+      //获取到当前上传的文件名字
+      let fileName = file.name;
+      //切割 分割 获取当前上传文件的后缀
+      let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+      //定一个检验是否通过的变量 初始值为false
+      let validateSuffix = false;
+      //循环数组 来判断 核对
+      for (let i = 0; i < suffixs.length; i++) {
+        //统一转成小写 来进行判断
+      if (suffixs[i].toLowerCase() === suffix) {
+        //校验通过 变量为true;
+        validateSuffix = true;
+        break;
+      }
+    }
+      //判断
+      if(!validateSuffix) {
+        Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+        console.log("文件格式不正确！只支持上传：" + suffixs.join(","));
+        return;
+      }
       //key："file"必须controller参数名一致
       formData.append('file', document.querySelector('#file-upload-input').files[0]);
       Loading.show();
